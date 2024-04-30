91602e34a2658c68aff353e7a4e4f87471d6f8f0
Anmol Jhamb <talktoanmol@outlook.com>
2024-04-30 22:05:40 +0530

    Added tailwind css and mui

---
+++ front-end/src/constants.ts
@@ -0,0 +1,8 @@
+import { createMuiTheme } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+export const appTheme = createMuiTheme({
+  palette: {
+    primary: purple,
+  },
+});
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import { appTheme } from "./constants.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <ThemeProvider theme={appTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </>,
+);
