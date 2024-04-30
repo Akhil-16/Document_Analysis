90826f3b199afe0f62f64ca61bcd09188c5d943e
Anmol Jhamb <talktoanmol@outlook.com>
2024-04-30 22:24:22 +0530

    Added auth provider

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -9,13 +9,16 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import { appTheme } from "./constants.ts";
+import AuthProvider from "./providers/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <ThemeProvider theme={appTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   </>,
 );
--- front-end/src/providers/AuthProvider.tsx
+++ front-end/src/providers/AuthProvider.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import LoadingPage from "../pages/LoadingPage";
 import { auth } from "../firebase";
+import { User } from "firebase/auth";
 
 type Props = {
   children: React.ReactNode;
@@ -9,19 +10,19 @@ type Props = {
 
 const AuthProvider = (props: Props) => {
   const [firebaseLoading, setFirebaseLoading] = useState<boolean>(true);
-  const [firestoreLoading, setFirestoreLoading] = useState<boolean>(true);
 
-  const [currentUser, setCurrentUser] = useState<null>(null);
+  const [currentUser, setCurrentUser] = useState<null | User>(null);
 
   useEffect(() => {
     const unsub = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user as null);
+      setCurrentUser(user);
+      setFirebaseLoading(false);
     });
 
     return unsub;
   }, []);
 
-  const loading = firebaseLoading || firestoreLoading;
+  const loading = firebaseLoading;
   const isLoggedIn = currentUser !== null;
 
   return (
